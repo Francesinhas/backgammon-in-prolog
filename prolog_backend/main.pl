bd01494453156c011cd69994975fd41ac23c9373
GeorgeTof <tofangeorge23@gmail.com>
2025-06-14 15:54:48 +0100

    implement new ai functionality in main

--- prolog_backend/main.pl
+++ prolog_backend/main.pl
@@ -24,6 +24,10 @@ ai_turn(Player) :-
     perform_move_from_bar(Player, To).
 
 ai_turn(Player) :-
+    choose_move_with_dice(Player, move(From, off)),
+    perform_bear_off(Player, From).
+
+ai_turn(Player) :-
     choose_move_with_dice(Player, move(From, To)),
     perform_move(Player, From, To).
 
@@ -47,14 +51,13 @@ perform_move_from_bar(Player, To) :-
     use_die(L).
 
 perform_bear_off(Player, Point) :-
-    % 1. Validate the move using the new logic and get the die that will be used.
     validate_and_get_bear_off_die(Player, Point, UsedDie),
-    
-    % 2. Apply the state change (remove checker from board and add to 'off').
     bear_off_piece(Player, Point),
-    
-    % 3. Consume the correct die from the current roll.
     use_die(UsedDie).
+
+
+
+
 
 perform_move_from_bar_dirty(Player, To) :-  % not used!
     bar(Player, BarCount), BarCount > 0,
